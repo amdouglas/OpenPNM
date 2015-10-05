5d0183e3e2af8cd903469ca9855280506a14963f
Ali Douglas <alistair.douglas13@gmail.com>
2015-10-05 16:36:50 +0100

    Updates to reservoir function

--- Reservoir data/por_perm_case2a/matrix2.py
+++ Reservoir data/por_perm_case2a/matrix2.py
@@ -1,22 +1,21 @@
 import numpy as np
 
-z = 220 #Dimensions
-x = 85
-y = 60
-a = np.arange(y) #1-D array
-
 data = np.loadtxt("spe_phi_sample.prn")
 
-print(data)
+print(data.shape)
 
-result = np.tile(data[None, :, None], (x, y, z))
-print (result.shape)
+data = data.reshape(85,60,2)
 
+print(data.shape)
 
-#y,z,x = result.nonzero()
+x,y,z = data.nonzero()
 
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
-#ax.scatter(x, y, -z, zdir='z', c= 'red')
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(-z, x, y, c=y ,  zdir='z')
+
+ax.set_xlabel('Z')
+ax.set_ylabel('X')
+ax.set_zlabel('Y')
